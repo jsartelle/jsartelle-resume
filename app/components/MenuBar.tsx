cb754b4043ff86be865e6230eced94b2237e282d
James Sartelle <james.sartelle@extra.app>
2024-06-01 01:05:29 -0500

    Menu bar

---
+++ app/components/MenuBar.tsx
@@ -0,0 +1,38 @@
+import styles from './MenuBar.module.css'
+
+export default function MenuBar() {
+  return (
+    <header className={styles.menubar}>
+      <hgroup>
+        <h1>James Sartelle</h1>
+        <details name="menubar">
+          <summary>About Me</summary>
+          <menu>
+            <li>Full Stack Engineer</li>
+            <li>Oklahoma City, OK</li>
+          </menu>
+        </details>
+        <details name="menubar">
+          <summary>Contact</summary>
+          <menu>
+            <li>
+              <a target="_blank" href="tel:9806229704">
+                Dial 980-622-9704...
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="mailto:jamessartelle@me.com">
+                Email jamessartelle@me.com...
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://github.com/jsartelle">
+                Open github.com/jsartelle...
+              </a>
+            </li>
+          </menu>
+        </details>
+      </hgroup>
+    </header>
+  )
+}

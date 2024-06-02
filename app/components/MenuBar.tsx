4d5abffa02fbdc6e062f3b5832f1b6e792add5eb
James Sartelle <james.sartelle@extra.app>
2024-06-02 14:54:44 -0500

    Reorganized menu bar

--- app/components/MenuBar.tsx
+++ app/components/MenuBar.tsx
@@ -1,46 +1,52 @@
 import styles from './MenuBar.module.css'
+import { UserRound, Printer } from 'lucide-react'
 
 export default function MenuBar() {
   return (
     <header className={styles.menubar}>
-      <hgroup>
-        <h1>James Sartelle</h1>
+      <div className={[styles.profileIcon, 'print-hidden'].join(' ')}>
+        <UserRound />
+      </div>
 
-        {/* Once CSS Anchor Positioning is widely available, the menubar can be changed to use popovers which will close if you click outside them. But alas, it's Chromium only for now */}
-        <details name="menubar">
-          <summary>About Me</summary>
-          <menu>
-            <li>Full Stack Engineer</li>
-            <li>Oklahoma City, OK</li>
-          </menu>
-        </details>
+      <h1 className={styles.name}>James Sartelle</h1>
 
-        <details name="menubar">
-          <summary>Contact</summary>
-          <menu>
-            <li>
-              <a target="_blank" href="tel:9806229704">
-                Dial 980-622-9704...
-              </a>
-            </li>
-            <li>
-              <a target="_blank" href="mailto:jamessartelle@me.com">
-                Email jamessartelle@me.com...
-              </a>
-            </li>
-            <li>
-              <a target="_blank" href="https://github.com/jsartelle">
-                Open GitHub (jsartelle)...
-              </a>
-            </li>
-            <li>
-              <a target="_blank" href="https://www.linkedin.com/in/james-sartelle/">
-                Open LinkedIn...
-              </a>
-            </li>
-          </menu>
-        </details>
-      </hgroup>
+      <button popoverTarget="aboutWindowPopover" className="print-hidden">
+        About Me
+      </button>
+
+      <details name="menubar" className="print-hidden">
+        <summary>Contact</summary>
+        <menu>
+          <li>
+            <a target="_blank" href="tel:9806229704">
+              Dial 980-622-9704...
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="mailto:jamessartelle@me.com">
+              Email jamessartelle@me.com...
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://github.com/jsartelle">
+              Open GitHub (jsartelle)...
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/james-sartelle/"
+            >
+              Open LinkedIn...
+            </a>
+          </li>
+        </menu>
+      </details>
+
+      {/* TODO make this a functional Print button */}
+      <button className={[styles.printButton, 'print-hidden'].join(' ')}>
+        <Printer />
+      </button>
     </header>
   )
 }

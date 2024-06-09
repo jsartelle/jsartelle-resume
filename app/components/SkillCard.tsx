3bb6c68ba8ffeae48ddee9234d0bb4ad82f6e1af
James Sartelle <james.sartelle@extra.app>
2024-06-09 15:11:55 -0500

    aria attributes for progress bar

--- app/components/SkillCard.tsx
+++ app/components/SkillCard.tsx
@@ -18,6 +18,10 @@ export default function SkillCard({
   secondary,
   progress,
 }: SkillCardProps) {
+  const headerId = `SkillCard_${title}_${(Math.random() + 1)
+    .toString(36)
+    .substring(7)}`
+
   const icon =
     (LogoIcon && <LogoIcon className={styles.logo} size={50} />) ??
     (logo && (
@@ -36,14 +40,20 @@ export default function SkillCard({
     <article className={styles.skillCard}>
       {icon}
       <hgroup>
-        <h4>{title}</h4>
+        <h4 id={headerId}>{title}</h4>
         {secondary ? <p className={styles.secondary}>{secondary}</p> : null}
       </hgroup>
       {/* <progress> elements are extremely difficult to style across browsers, and animation support is limited */}
       <div
         className={[styles.progress, 'print-hidden'].join(' ')}
-        role='progressbar'
-        style={{'--value-width': `${progress / 5 * 100}%`} as React.CSSProperties}
+        role="progressbar"
+        aria-labelledby={headerId}
+        aria-valuemin={0}
+        aria-valuenow={progress}
+        aria-valuemax={5}
+        style={
+          { '--value-width': `${(progress / 5) * 100}%` } as React.CSSProperties
+        }
       >
         <div className={styles.progressValue}></div>
       </div>

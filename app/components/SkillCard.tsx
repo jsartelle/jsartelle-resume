1ff87fa272ec4ac105539983e46529b67b979921
James Sartelle <james.sartelle@extra.app>
2024-06-11 19:26:04 -0500

    Update SkillCard props

--- app/components/SkillCard.tsx
+++ app/components/SkillCard.tsx
@@ -1,26 +1,23 @@
-import type { ComponentType } from 'react'
+import type { ComponentType, PropsWithChildren } from 'react'
 import Image, { type StaticImageData } from 'next/image'
 import type { LucideProps } from 'lucide-react'
 import styles from './SkillCard.module.css'
 
-export interface SkillCardProps {
+export interface SkillCardProps extends PropsWithChildren {
   logo?: StaticImageData
   LogoIcon?: ComponentType<LucideProps>
-  title: string
-  secondary?: string
+  subtitle?: string
   progress: 0 | 1 | 2 | 3 | 4 | 5
 }
 
 export default function SkillCard({
   logo,
   LogoIcon,
-  title,
-  secondary,
+  subtitle,
   progress,
+  children,
 }: SkillCardProps) {
-  const headerId = `SkillCard_${title}_${(Math.random() + 1)
-    .toString(36)
-    .substring(7)}`
+  const headerId = `SkillCard_${(Math.random() + 1).toString(36).substring(7)}`
 
   const icon =
     (LogoIcon && <LogoIcon className={styles.logo} size={50} />) ??
@@ -40,9 +37,8 @@ export default function SkillCard({
     <article className={styles.skillCard}>
       {icon}
       <hgroup>
-        {/* FIXME should use children for this */}
-        <h4 id={headerId}>{title}</h4>
-        {secondary ? <p className={styles.secondary}>{secondary}</p> : null}
+        <h4 id={headerId}>{children}</h4>
+        {subtitle ? <p className={styles.subtitle}>{subtitle}</p> : null}
       </hgroup>
       {/* <progress> elements are extremely difficult to style across browsers, and animation support is limited */}
       <div

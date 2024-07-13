0f6848cd83ea038b2adbcc49b2e4039b93e90d51
James Sartelle <james.sartelle@extra.app>
2024-07-13 01:07:15 -0500

    Rename inactive listener dataset property

--- app/components/WindowInactive.tsx
+++ app/components/WindowInactive.tsx
@@ -4,8 +4,8 @@ import { useEffect } from 'react'
 /* this is implemented as a component instead of a hook so that it can be used in Server Components */
 export default function WindowInactive() {
   useEffect(() => {
-    if (!document.documentElement.dataset.windowInactive) {
-      document.documentElement.dataset.windowInactive = 'true'
+    if (!document.documentElement.dataset.inactiveListener) {
+      document.documentElement.dataset.inactiveListener = 'true'
 
       const setInactiveClass = () =>
         document.documentElement.classList.add('window-inactive')
@@ -18,7 +18,7 @@ export default function WindowInactive() {
       window.addEventListener('blur', setInactiveClass)
 
       return () => {
-        document.documentElement.removeAttribute('data-window-inactive')
+        document.documentElement.removeAttribute('data-inactive-listener')
         window.removeEventListener('focus', clearInactiveClass)
         window.removeEventListener('blur', setInactiveClass)
       }
